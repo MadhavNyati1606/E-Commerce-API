4bac92f3b43c6814ceb162fb2770ad16b04b18ff
Madhav Nyati <madhavnyati1606@gmail.com>
2024-12-17 23:39:23 +0530

    Fixed the bug in the not found middleware due to which the error handling was not working properly.

--- middleware/adminTokenAuthorization.js
+++ middleware/adminTokenAuthorization.js
@@ -13,16 +13,23 @@ const adminTokenAuth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if (payload.role === "admin") {
-      req.user = {
-        userId: payload.userId,
-        email: payload.email,
-        role: payload.role,
-      };
-      next();
+    if (payload.role !== "admin") {
+      throw new UnauthenticatedError(
+        "Authorization Invalid. Only admin users are allowed"
+      );
     }
+    req.user = {
+      userId: payload.userId,
+      email: payload.email,
+      role: payload.role,
+    };
+    next();
   } catch (err) {
-    throw new UnauthenticatedError("Authorization Invalid second");
+    if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
+      throw new UnauthenticatedError("Authorization Invalid");
+    } else {
+      throw err;
+    }
   }
 };
 
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,5 +1,5 @@
 const notFound = (req, res, next) => {
-  return res.status(404).json({ message: "The route does not exist." });
+  return res.status(404).send("The route does not exist.");
 };
 
 module.exports = notFound;
