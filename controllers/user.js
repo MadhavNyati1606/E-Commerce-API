a01b1d38cd629f2889e518b2649e19e4a3bc2d21
Madhav Nyati <madhavnyati1606@gmail.com>
2024-12-18 00:34:26 +0530

    Added both the string and the numeric filtering of the products for the user.

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,40 @@ const Product = require("../models/product");
 const { NotFoundError } = require("../errors");
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
+  const { category, isFeatured, name, numericFilters } = req.query;
+  const queryObject = {};
+  const operatorMap = {
+    ">=": "$gte",
+    ">": "$gt",
+    "=": "$eq",
+    "<": "$lt",
+    "<=": "$lte",
+    "!=": "$ne",
+  };
+
+  if (numericFilters) {
+    const filters = numericFilters
+      .split(",")
+      .map((item) => item.replace(/([a-zA-Z]+)([<>=!]+)(.+)/, "$1_$2_$3"));
+    for (let i = 0; i < filters.length; i++) {
+      const [field, operator, value] = filters[i].split("_");
+      queryObject[field] = { [operatorMap[operator]]: value };
+      console.log(queryObject);
+    }
+  }
+
+  if (category) {
+    queryObject.category = { $regex: category, $options: "i" };
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  if (isFeatured) {
+    queryObject.isFeatured = isFeatured;
+  }
+  const products = await Product.find(queryObject);
   res.status(StatusCodes.OK).json({ products });
 };
 
