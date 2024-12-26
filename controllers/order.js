67e291f72092c9c31bcbd6beca5c2afb1152b21d
Madhav Nyati <madhavnyati1606@gmail.com>
2024-12-26 23:29:02 +0530

    Added the functionality that the user cannot order out of stock products.

--- controllers/order.js
+++ controllers/order.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 const Order = require("../models/order");
 const Cart = require("../models/cart");
 const { BadRequestError } = require("../errors");
+const Product = require("../models/product");
 const getAllOrders = async (req, res) => {
   const orders = await Order.find({ user_id: req.user.userId });
   res.status(StatusCodes.OK).json({ orders });
@@ -12,6 +13,20 @@ const placeOrder = async (req, res) => {
 
   if (!cart || cart.items.length === 0) {
     throw new BadRequestError("Please add items to cart");
+  }
+
+  for (let i = 0; i < cart.items.length; i++) {
+    let productId = cart.items[i].product_id;
+    let product = await Product.findOne({ _id: productId });
+    console.log(product);
+    if (product.stock >= cart.items[i].quantity) {
+      product.stock -= cart.items[i].quantity;
+      await product.save();
+    } else {
+      throw new BadRequestError(
+        "This much quantity is not available for this product. Please select less quantity"
+      );
+    }
   }
 
   let orderItem = {
